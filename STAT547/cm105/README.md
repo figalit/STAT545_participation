6db2776751c62e3c8de0ab6536926df686170732
Figali Taho <figalitaho@gmail.com>
2018-11-08 23:39:07 -0800

    participation 4-5

--- STAT547/cm105/README.md
+++ STAT547/cm105/README.md
@@ -1,1 +1,3 @@
 # Participation folder for class meeting. 
+
+`powers` directory contains powers package related work. 
---
+++ STAT547/cm105/powers/powers/R/square.R
@@ -0,0 +1,6 @@
+#' @title Apply a power
+#' @param x Vector of numerics
+#' @return The vector \code{x}, with the corresponding power applied.
+#' @rdname common_doc
+#' @export
+square <- function(x) pow(x, 2)
